b9a615e13cbd5c16c2711a2e3092cd506bf4193a
серега24-41 <cool.dnevnik-ru2013@yandex.ru>
2025-05-20 14:55:53 +0300

    .

--- Lab4/lab4.js
+++ Lab4/lab4.js
@@ -1,11 +1,10 @@
-// Все предыдущие функции (getDecimal, normalizeUrl и т.д.)...
-
-// Конструктор Book
 function Book(title, pubYear, price) {
+    // Приватные свойства
     let _title = title;
     let _pubYear = pubYear;
     let _price = price;
 
+    // Метод show
     this.show = function() {
         console.log(`Название: ${_title}, Цена: ${_price}`);
     };
@@ -44,10 +43,24 @@ function Book(title, pubYear, price) {
     });
 }
 
+const book1 = new Book('JavaScript', 2020, 1000);
+book1.show();
+
+book1.title = 'New Title';
+book1.pubYear = 2023;
+book1.price = 1500;
 Book.compare = function(a, b) {
     return a.pubYear - b.pubYear;
 };
 
+const books = [
+    new Book('Book A', 2010, 500),
+    new Book('Book B', 2005, 300),
+    new Book('Book C', 2020, 700)
+];
+
+books.sort(Book.compare);
+console.log(books);
 function isEmpty(obj) {
     for (const key in obj) {
         if (obj.hasOwnProperty(key)) {
@@ -57,12 +70,40 @@ function isEmpty(obj) {
     return Object.getOwnPropertySymbols(obj).length === 0;
 }
 
+const objWithSymbol = { [Symbol()]: true };
+console.log(isEmpty(objWithSymbol));
+console.log(isEmpty({}));
+const obj = {
+    className: 'open menu',
+    addClass(cls) {
+        if (!this.className.includes(cls)) {
+            this.className += ` ${cls}`;
+        }
+        return this;
+    },
+    removeClass(cls) {
+        this.className = this.className
+            .split(' ')
+            .filter(c => c !== cls)
+            .join(' ');
+        return this;
+    }
+};
+
+obj.addClass('new').removeClass('open');
+console.log(obj.className);
+const jsonStr = JSON.stringify(obj, null, 2);
+console.log(jsonStr);
+
+const obj2 = JSON.parse(jsonStr);
+console.log(JSON.stringify(obj) === JSON.stringify(obj2));
 function getSecondsToday() {
     const now = new Date();
     const todayStart = new Date(now.getFullYear(), now.getMonth(), now.getDate());
     return Math.floor((now - todayStart) / 1000);
 }
 
+console.log(getSecondsToday());
 function formatDate(date) {
     const day = String(date.getDate()).padStart(2, '0');
     const month = String(date.getMonth() + 1).padStart(2, '0');
@@ -70,17 +111,4 @@ function formatDate(date) {
     return `${day}.${month}.${year}`;
 }
 
-export {
-    getDecimal,
-    normalizeUrl,
-    checkSpam,
-    truncate,
-    camelize,
-    fibs,
-    arrReverseSorted,
-    unique,
-    Book,
-    isEmpty,
-    getSecondsToday,
-    formatDate
-};
+console.log(formatDate(new Date()));
